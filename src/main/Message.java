cba9f6ed1851169a9f804757f9c7bdfb9a6bf37e
yiftachsa <yiftachs@post.bgu.ac.il>
2020-01-04 11:27:18 +0200

    Message class updated

--- src/main/Message.java
+++ src/main/Message.java
@@ -1,3 +1,9 @@
 public class Message {
     //TODO:Message
+    private char[] teamName;
+    private char type;
+    private char[] hash;
+    private char originalLength;
+    private char[] originalStringStart;
+    private char[] originalStringEnd;
 }
